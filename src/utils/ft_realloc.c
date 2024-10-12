4ccb3193b1c68d49f50c6bfaae3180b517a04ced
flmarsou <marsouinflorent12@gmail.com>
2024-10-12 20:17:39 +0200

    Minor Update

--- src/utils/ft_realloc.c
+++ src/utils/ft_realloc.c
@@ -12,19 +12,16 @@
 
 #include "minishell.h"
 
-unsigned char	*ft_realloc(unsigned char *old_str, unsigned int old_length,
-			unsigned int new_length)
+unsigned char	*ft_realloc(unsigned char *old_str, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int	i;
 	unsigned char	*new_str;
 
-	new_str = (unsigned char)malloc(sizeof(char) * (new_length + 1));
+	new_str = (unsigned char *)malloc(sizeof(unsigned char) * (new_size + 1));
 	if (new_str == NULL)
 		ft_perror(1);
-	if (old_length >= new_length)
-		ft_perror(0);
 	i = 0;
-	while (i < old_length)
+	while (i < old_size)
 	{
 		new_str[i] = old_str[i];	
 		i++;

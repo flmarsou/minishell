604cca6116d2bb1b8084bf8d5ce6b77c67d493a1
flmarsou <marsouinflorent12@gmail.com>
2024-10-24 15:47:13 +0200

    Better Core Loop

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:45:48 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/10/21 13:58:41 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/10/24 14:19:08 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 
 int	main(void)
 {
-	read_user_input();
+	struct termios	orig_termios;
+	t_input			input;
+
+	enable_raw_mode(&orig_termios);
+	while (true)
+	{
+		read_user_input(&input);
+		printf("%s | %u | %u\n\n", input.buffer, input.length, input.cursor_pos); // Debug
+		if (input.buffer)
+			free(input.buffer);
+	}
+	disable_raw_mode(&orig_termios);
 	return (0);
 }

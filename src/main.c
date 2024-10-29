9e1c4de3be5f7f14b25194d8be5984b8e0e21055
Manioker <andithevac@gmail.com>
2024-10-29 15:50:39 +0100

    Added debug print

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anvacca <anvacca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:45:48 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/10/28 13:27:10 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/10/29 15:33:13 by anvacca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,22 @@ int	main(void)
 {
 	struct termios	orig_termios;
 	t_input			input;
+	t_lexer			lexer;
 
 	enable_raw_mode(&orig_termios);
 	get_terminal_size(&input);
 	while (true)
 	{
 		read_user_input(&input);
+		tokenizer(input.buffer, &lexer);		
 		printf("%s | Lenght: %u | Cursor X: %u Y: %u\n", input.buffer, input.len, input.cursor_x, input.cursor_y); // Debug
 		printf("Win X: %u Y: %u\n\n", input.term_x, input.term_y); // Debug
+		for (size_t i = 0; lexer.str[i]; i++)
+		{
+			printf("%s %d\n", lexer.str[i], i);
+			free(lexer.str[i]);
+		}
+		free(lexer.str);
 		if (input.buffer)
 			free(input.buffer);
 	}

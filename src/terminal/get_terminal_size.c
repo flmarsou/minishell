cec3b5106e4a960f572cf2b2831817afcccc61a2
flmarsou <marsouinflorent12@gmail.com>
2024-10-31 13:50:50 +0100

    Sigaction to Signal

--- src/terminal/get_terminal_size.c
+++ src/terminal/get_terminal_size.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 12:53:21 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/10/28 14:26:49 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/10/30 15:37:32 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,26 +30,21 @@ static void	handle_winsize(void)
 	if (ioctl(STDOUT, TIOCGWINSZ, &ws) == -1)
 		ft_perror(2);
 	input->term_x = ws.ws_col;
-	input->term_y = ws.ws_row;
 }
 
-static void	handle_sigwinch(int sig, siginfo_t *info, void *context)
+static void	handle_sigwinch(int sig)
 {
 	(void)sig;
-	(void)info;
-	(void)context;
 	handle_winsize();
 }
 
 void	get_terminal_size(t_input *input)
 {
-	struct sigaction	sa;
+	struct winsize	ws;
 
-	get_input_instance(input);
-	sa.sa_sigaction = handle_sigwinch;
-	sa.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa.sa_mask);
-	if (sigaction(SIGWINCH, &sa, NULL) == -1)
+	if (ioctl(STDOUT, TIOCGWINSZ, &ws) == -1)
 		ft_perror(2);
-	handle_winsize();
+	input->term_x = ws.ws_col;
+	get_input_instance(input);
+	signal(SIGWINCH, handle_sigwinch);
 }

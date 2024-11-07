39e2bf35742f4035bffa9476773e221d924a8b4e
flmarsou <marsouinflorent12@gmail.com>
2024-11-07 12:52:58 +0100

    New main with readline

--- src/main.c
+++ src/main.c
@@ -6,20 +6,47 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:45:48 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/11/06 12:09:18 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/11/07 12:51:39 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	handle_signal(int sig)
+{
+	if (sig == SIGINT)
+	{
+		write(STDOUT, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+}
+
+static void	free_all(char *buffer)
+{
+	if (buffer)
+		free(buffer);
+}
+
 int	main(void)
 {
-	t_input			input;
-	t_lexer			lexer;
+	char	*buffer;
+	t_lexer	lexer;
 
+	signal(SIGINT, handle_signal);
 	while (true)
 	{
-		return (0);
+		buffer = readline("Nanashell > ");
+		if (!buffer || ft_strncmp(buffer, "exit", 4))
+			break ;
+		if (buffer)
+			add_history(buffer);
+		// tokenizer((unsigned char *)buffer, &lexer);
+		free(buffer);
 	}
+	free_all(buffer);
+	rl_clear_history();
+	write(STDOUT, "Exiting...\n", 11);
 	return (0);
 }

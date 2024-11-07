b89efb8775406bfe66be9919970cd713d94647e0
flmarsou <marsouinflorent12@gmail.com>
2024-11-07 15:50:51 +0100

    Int to *int

--- src/lexer/tokenizer.c
+++ src/lexer/tokenizer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenizer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anvacca <anvacca@student.42.fr>            +#+  +:+       +#+        */
+/*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 13:20:02 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/11/07 15:28:42 by anvacca          ###   ########.fr       */
+/*   Updated: 2024/11/07 15:47:53 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@ void	handle_double_meta(unsigned char *input, t_lexer *lexer,
 		unsigned int *x, unsigned int *y)
 {
 	if (&input[*x] == '>')
-		lexer->token = APPEND_REDIRECT;
+		lexer->token[*y] = APPEND_REDIRECT;
 	else
-		lexer->token = HEREDOC;
+		lexer->token[*y] = HEREDOC;
 	lexer->str[*y] = malloc(sizeof(char) * 3);
 	ft_strcpy(lexer->str[*y], &input[*x], 2);
-	lexer->str[*y][2] = '\0';
 	*y += 1;
 	*x += 2;
 }
@@ -30,20 +29,19 @@ void	handle_single_meta(unsigned char *input, t_lexer *lexer,
 		unsigned int *x, unsigned int *y)
 {
 	if (&input[*x] == '\'')
-		lexer->token = SINGLE_QUOTE;
+		lexer->token[*y] = SINGLE_QUOTE;
 	else if (&input[*x] == '\"')
-		lexer->token = DOUBLE_QUOTE;
+		lexer->token[*y] = DOUBLE_QUOTE;
 	else if (&input[*x] == '|')
-		lexer->token = PIPE;
+		lexer->token[*y] = PIPE;
 	else if (&input[*x] == '>')
-		lexer->token = OUTPUT_REDIRECT;
+		lexer->token[*y] = 666;
 	else if (&input[*x] == '<')
-		lexer->token = INPUT_REDIRECT;
+		lexer->token[*y] = INPUT_REDIRECT;
 	else if (&input[*x] == '$')
-		lexer->token = DOLLAR;
+		lexer->token[*y] = DOLLAR;
 	lexer->str[*y] = malloc(sizeof(char) * 2);
 	ft_strcpy(lexer->str[*y], &input[*x], 1);
-	lexer->str[*y][1] = '\0';
 	*y += 1;
 	*x += 1;
 }
@@ -51,10 +49,9 @@ void	handle_single_meta(unsigned char *input, t_lexer *lexer,
 void	handle_whitespace(unsigned char *input, t_lexer *lexer, unsigned int *x,
 		unsigned int *y)
 {
-	lexer->token = SEPARATOR;
+	lexer->token[*y] = SEPARATOR;
 	lexer->str[*y] = malloc(sizeof(char) * 2);
 	ft_strcpy(lexer->str[*y], &input[*x], 1);
-	lexer->str[*y][1] = '\0';
 	*y += 1;
 }
 
@@ -64,13 +61,12 @@ void	handle_commands(unsigned char *input, t_lexer *lexer, unsigned int *x,
 	unsigned int	i;
 
 	i = 0;
-	lexer->token = COMMAND;
+	lexer->token[*y] = COMMAND;
 	while (ft_isprint(input[*x + i]) && !ft_ismeta(input[*x + i])
 		&& !ft_isspace(input[*x + i]))
 		i++;
 	lexer->str[*y] = malloc(sizeof(char) * i + 1);
 	ft_strcpy(lexer->str[*y], &input[*x], i);
-	lexer->str[*y][i] = '\0';
 	*x += i;
 	*y += 1;
 }
@@ -83,6 +79,7 @@ void	tokenizer(unsigned char *input, t_lexer *lexer)
 	x = 0;
 	y = 0;
 	lexer->str = malloc(sizeof(char *) * (count_tokens(input) + 1));
+	lexer->token = malloc(sizeof(int) * (count_tokens(input) + 1));
 	while (ft_isspace(input[x]))
 		x++;
 	while (input[x])

68bcc869c0d211261c35a03970ac198651dbce11
Manioker <andithevac@gmail.com>
2024-11-12 13:27:08 +0100

    Finished ft_env

--- src/builtins/ft_env.c
+++ src/builtins/ft_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_env.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anvacca <anvacca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 10:25:55 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/11/11 15:18:51 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/11/12 13:26:26 by anvacca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 void	ft_env(t_environ *environ)
 {
-	unsigned int	i;
-
-	i = 0;
 	while (environ)
 	{
-		printf("%s\n", (*environ).var);
-		environ = (*environ).next;
+		if (!strchr(environ->var, '='))
+			environ = (*environ).next;
+		else
+		{
+			printf("%s\n", (*environ).var);
+			environ = (*environ).next;
+		}
 	}
 }

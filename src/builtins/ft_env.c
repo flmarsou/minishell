0314141c82aeea286183ad8ad9bdb6a63aafbde2
flmarsou <marsouinflorent12@gmail.com>
2024-11-12 15:31:06 +0100

    Don't forget to add ft_strchr

--- src/builtins/ft_env.c
+++ src/builtins/ft_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_env.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anvacca <anvacca@student.42.fr>            +#+  +:+       +#+        */
+/*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 10:25:55 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/11/12 13:26:26 by anvacca          ###   ########.fr       */
+/*   Updated: 2024/11/12 15:01:10 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_env(t_environ *environ)
 {
 	while (environ)
 	{
-		if (!strchr(environ->var, '='))
+		if (!strchr(environ->var, '=')) // Add ft_strchr into Utils
 			environ = (*environ).next;
 		else
 		{

58896f85c0514a4cf8955f6bb3176badcc648295
flmarsou <marsouinflorent12@gmail.com>
2024-11-29 10:15:29 +0100

    Fixed Quotes Parser

--- src/parser/parse_quotes.c
+++ src/parser/parse_quotes.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 12:55:45 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/11/26 12:57:11 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/11/29 10:13:31 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ bool	handle_quote_error(t_lexer lexer)
 	unsigned int	quote;
 
 	i = 0;
-	quote = 0;
-	while (lexer.token[i])
+	quote = 42;
+	while (lexer.str[i])
 	{
-		if (quote == 0 && (lexer.token[i] == SINGLE_QUOTE
-				|| lexer.token[i] == DOUBLE_QUOTE))
+		if (quote == 42 && (lexer.token[i] == SINGLE_QUOTE
+			|| lexer.token[i] == DOUBLE_QUOTE))
 			quote = lexer.token[i];
-		else if (quote != 0 && lexer.token[i] == quote)
-			quote = 0;
+		else if (quote != 42 && lexer.token[i] == quote)
+			quote = 42;
 		i++;
 	}
-	if (quote)
+	if (quote != 42)
 		return (ft_strerror("Wrong quote placement!"), false);
 	return (true);
 }

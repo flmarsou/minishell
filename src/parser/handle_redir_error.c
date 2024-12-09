1e3b26939009f7f1090c74fab3269e87d7e3c70a
flmarsou <marsouinflorent12@gmail.com>
2024-12-09 16:00:15 +0100

    Added redirection parser

--- src/parser/handle_redir_error.c
+++ src/parser/handle_redir_error.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 12:26:14 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/12/04 12:55:19 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/12/09 10:41:38 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,29 @@
 
 bool	handle_redir_error(t_lexer lexer)
 {
-	(void)lexer;
+	unsigned int	i;
+	bool			quote;
+
+	i = 0;
+	quote = false;
+	while (lexer.str[i])
+	{
+		if (!quote && (lexer.token[i] == INPUT_REDIRECT
+				|| lexer.token[i] == OUTPUT_REDIRECT
+				|| lexer.token[i] == HEREDOC
+				|| lexer.token[i] == APPEND_REDIRECT))
+			quote = true;
+		else if (quote && (lexer.token[i] == INPUT_REDIRECT
+				|| lexer.token[i] == OUTPUT_REDIRECT
+				|| lexer.token[i] == HEREDOC
+				|| lexer.token[i] == APPEND_REDIRECT
+				|| lexer.token[i] == PIPE))
+			break ;
+		else if (quote && lexer.token[i] == WORD)
+			quote = false;
+		i++;
+	}
+	if (quote)
+		return (ft_strerror("Syntax error, unexpected redirection"), false);
 	return (true);
 }

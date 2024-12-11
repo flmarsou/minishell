4e9d06dfdcf4d6f1c59233c00eb35ab0af2cfb5e
flmarsou <marsouinflorent12@gmail.com>
2024-12-11 14:09:21 +0100

    Fixed destroy_token

--- src/parser/_destroy_token.c
+++ src/parser/_destroy_token.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   destroy_token.c                                    :+:      :+:    :+:   */
+/*   _destroy_token.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 10:57:47 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/12/10 10:58:06 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/12/11 13:21:39 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	destroy_token(t_lexer *lexer, unsigned int y)
 {
 	unsigned int	i;
 
-	i = 0;
+	i = 1;
 	lexer->token[y] = NA_VALUE;
 	lexer->str[y][0] = '\a';
 	while (lexer->str[y][i])

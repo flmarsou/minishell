21cc351562db0f65b772a2873a582a12200f8ff4
flmarsou <marsouinflorent12@gmail.com>
2024-12-11 14:08:52 +0100

    Finished handle_redir

--- src/parser/handle_redir.c
+++ src/parser/handle_redir.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 10:30:36 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/12/10 14:53:02 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/12/11 13:44:43 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,15 +40,68 @@ static unsigned int	count_redirs(t_lexer lexer, bool reset)
 	return (count);
 }
 
+void	found_redir(t_lexer *lexer, t_parser *parser, unsigned int *i, bool reset)
+{
+	static unsigned int	index = 0;
+
+	if (reset)
+	{
+		index = 0;
+		return ;
+	}
+	parser->redir.token[index] = lexer->token[*i];
+	destroy_token(lexer, *i);
+	while (lexer->str[*i] && lexer->token[*i] != WORD)
+		(*i)++;
+	parser->redir.type[index] = ft_strdup(lexer->str[*i]);
+	destroy_token(lexer, *i);
+	index++;
+}
+
+// void	print_parser(t_parser parser)
+// {
+// 	unsigned int	i;
+
+// 	i = 0;
+// 	while (i < parser.redir.nbr_of_redirs)
+// 	{
+// 		fprintf(stderr, "Redir.token[i] = %d\n", parser.redir.token[i]);
+// 		fprintf(stderr, "Redir.type[i] = %s\n", parser.redir.type[i]);
+// 		i++;
+// 	}
+	
+// }
+
 void	handle_redir(t_lexer *lexer, t_parser *parser, unsigned int groups)
 {
 	unsigned int	i;
+	unsigned int	group;
 
 	i = 0;
 	while (i < groups)
 	{
 		parser[i].redir.nbr_of_redirs = count_redirs(*lexer, false);
+		parser[i].redir.token = malloc(sizeof(t_tokens) * (parser[i].redir.nbr_of_redirs));
+		parser[i].redir.type = malloc(sizeof(char *) * (parser[i].redir.nbr_of_redirs + 1));
+		parser[i].redir.type[parser[i].redir.nbr_of_redirs] = NULL;
 		i++;
 	}
 	count_redirs(*lexer, true);
+	i = 0;
+	group = 0;
+	while(lexer->str[i])
+	{
+		if (lexer->token[i] == PIPE)
+		{
+			group++;
+			found_redir(lexer, &parser[group], &i, true);
+		}
+		if (lexer->token[i] == INPUT_REDIRECT
+				|| lexer->token[i] == OUTPUT_REDIRECT
+				|| lexer->token[i] == HEREDOC
+				|| lexer->token[i] == APPEND_REDIRECT)
+			found_redir(lexer, &parser[group], &i, false);
+		else
+			i++;
+	}
 }

01dce2af04fc6ec4675b5bed8270bd0b67c2598e
flmarsou <marsouinflorent12@gmail.com>
2024-12-16 12:24:10 +0100

    Norminette

--- src/parser/handle_redir.c
+++ src/parser/handle_redir.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 10:30:36 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/12/13 15:27:45 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/12/16 12:16:04 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,28 +52,13 @@ static void	alloc_redir(t_lexer *lexer, t_parser *parser, unsigned int groups)
 	}
 }
 
-static void	loop(t_lexer *lexer, t_parser *parser, unsigned int *i)
+static void	nanachi(t_lexer *lexer, unsigned int *i, unsigned int *group,
+	unsigned int *index)
 {
-	unsigned int	index;
-	unsigned int	group;
-
-	index = 0;
-	group = 0;
-	if (lexer->token[*i] == INPUT_REDIRECT
-		|| lexer->token[*i] == OUTPUT_REDIRECT
-		|| lexer->token[*i] == HEREDOC
-		|| lexer->token[*i] == APPEND_REDIRECT)
+	if (lexer->token[*i] == PIPE)
 	{
-		parser[group].token[index] = lexer->token[*i];
-		while (lexer->str[*i] && lexer->token[*i] != WORD)
-			(*i)++;
-		parser[group].type[index] = strdup(lexer->str[*i]);
-		index++;
-	}
-	else if (lexer->token[*i] == PIPE)
-	{
-		index = 0;
-		group++;
+		*index = 0;
+		(*group)++;
 		(*i)++;
 	}
 	else
@@ -83,9 +68,28 @@ static void	loop(t_lexer *lexer, t_parser *parser, unsigned int *i)
 void	handle_redir(t_lexer *lexer, t_parser *parser, unsigned int groups)
 {
 	unsigned int	i;
+	unsigned int	group;
+	unsigned int	index;
 
 	alloc_redir(lexer, parser, groups);
 	i = 0;
+	group = 0;
+	index = 0;
 	while (lexer->str[i])
-		loop(lexer, parser, &i);
+	{
+		if (lexer->token[i] == INPUT_REDIRECT
+			|| lexer->token[i] == OUTPUT_REDIRECT
+			|| lexer->token[i] == HEREDOC
+			|| lexer->token[i] == APPEND_REDIRECT)
+		{
+			parser[group].token[index] = lexer->token[i];
+			while (lexer->str[i] && lexer->token[i] != WORD)
+				i++;
+			parser[group].type[index] = ft_strdup(lexer->str[i]);
+			lexer->token[i] = NA_VALUE;
+			index++;
+		}
+		else
+			nanachi(lexer, &i, &group, &index);
+	}
 }

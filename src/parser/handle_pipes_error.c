f533bcb915c08165cd30378bb8de57105f46e5d8
flmarsou <marsouinflorent12@gmail.com>
2024-12-23 14:53:06 +0100

    Better pipe error handler

--- src/parser/handle_pipes_error.c
+++ src/parser/handle_pipes_error.c
@@ -6,46 +6,31 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 09:56:41 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/12/05 10:47:40 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/12/23 14:41:04 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static bool	check_input(t_lexer lexer, bool *found_pipe)
-{
-	unsigned int	i;
-	bool			found_word;
-
-	i = 0;
-	while (lexer.str[i])
-	{
-		found_word = false;
-		if (lexer.token[i] == PIPE)
-		{
-			if (*found_pipe)
-				return (false);
-			*found_pipe = true;
-		}
-		else if (lexer.token[i] == WORD)
-		{
-			found_word = true;
-			*found_pipe = false;
-		}
-		i++;
-	}
-	return (true);
-}
-
 bool	handle_pipes_error(t_lexer lexer)
 {
-	bool	found_pipe;
+	unsigned int	i;
+	bool			found_pipe;
 
+	i = 0;
 	found_pipe = false;
-	if (lexer.token[0] == PIPE)
-		return (ft_strerror("Syntax error, unexpected `|`"), false);
-	if (!check_input(lexer, &found_pipe))
-		return (ft_strerror("Syntax error, unexpected `|`"), false);
+	while (lexer.str[i])
+	{
+		if (lexer.token[i] == PIPE)
+		{
+			if (found_pipe)
+				return (ft_strerror("Syntax error, unexpected `|`"), false);
+			found_pipe = true;
+		}
+		else if (lexer.token[i] == WORD)
+			found_pipe = false;
+		i++;
+	}
 	if (found_pipe)
 		return (ft_strerror("Syntax error, unexpected `|`"), false);
 	return (true);

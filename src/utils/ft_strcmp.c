dd3cbeb2a90385050e866a339a317e7845595168
flmarsou <marsouinflorent12@gmail.com>
2024-12-24 15:24:11 +0100

    Updated ft_strcmp

--- src/utils/ft_strcmp.c
+++ src/utils/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 13:13:15 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/11/25 11:44:00 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/12/24 15:19:12 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ bool	ft_strcmp(char *str1, char *str2)
 	unsigned int	i;
 
 	i = 0;
+	if (str1[0] == '\0')
+		return (false);
 	while (str1[i] && str2[i])
 	{
 		if (str1[i] != str2[i])

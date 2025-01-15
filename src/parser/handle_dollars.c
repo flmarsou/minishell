0fd81fa99e1e0a6b107a9272f20f818630ca7a7b
flmarsou <marsouinflorent12@gmail.com>
2025-01-15 15:53:09 +0100

    Added Dollar Features

--- src/parser/handle_dollars.c
+++ src/parser/handle_dollars.c
@@ -6,61 +6,84 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 14:13:08 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/12/09 14:11:12 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/01/15 15:27:51 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static char	*find_in_env(char *var, t_environ environ)
+static char	*find_in_env(char *var, char **envp)
 {
 	unsigned int	len;
+	unsigned int	i;
 
 	len = ft_strlen(var);
-	while (environ.next)
+	i = 0;
+	while (envp[i])
 	{
-		if (ft_strncmp(environ.var, var, ft_strlen(var))
-			&& environ.var[len] == '=')
-			return (environ.var + ft_strlen(var) + 1);
-		environ = *environ.next;
+		if (ft_strncmp(envp[i], var, ft_strlen(var)) && envp[i][len] == '=')
+			return (envp[i] + ft_strlen(var) + 1);
+		i++;
 	}
 	return (NULL);
 }
 
-void	found_var(t_lexer *lexer, unsigned int i, char *var)
+static void	handle_dollar_exit(t_lexer *lexer, unsigned int *i)
 {
-	lexer->str[i] = malloc(sizeof(char) * (ft_strlen(var) + 1));
-	ft_strcpy(lexer->str[i], var);
+	char	*exit_status;
+	char	*buffer;
+
+	lexer->token[*i] = NA_VALUE;
+	(*i)++;
+	exit_status = ft_itoa(g_exit_status);
+	buffer = ft_strdup(lexer->str[*i]);
+	free(lexer->str[*i]);
+	lexer->str[*i] = ft_strjoin(exit_status, buffer + 1);
+	free(exit_status);
+	free(buffer);
 }
 
-void	no_var(t_lexer *lexer, unsigned int i)
+static void	handle_dollar_word(t_lexer *lexer, unsigned int *i, char **envp)
 {
-	lexer->str[i] = malloc(sizeof(char) * 2);
-	lexer->str[i][0] = '\a';
-	lexer->str[i][1] = '\0';
-	lexer->token[i] = NA_VALUE;
+	char	*var;
+
+	lexer->token[*i] = NA_VALUE;
+	(*i)++;
+	var = find_in_env(lexer->str[*i], envp);
+	if (var)
+	{
+		free(lexer->str[*i]);
+		lexer->str[*i] = malloc(sizeof(char) * (ft_strlen(var) + 1));
+		ft_strcpy(lexer->str[*i], var);
+	}
+	else
+		lexer->token[*i] = NA_VALUE;
 }
 
-void	handle_dollars(t_lexer *lexer, t_environ environ)
+void	handle_dollars(t_lexer *lexer, char **envp)
 {
 	unsigned int	i;
-	char			*var;
 
 	i = 0;
-	while (lexer->str[i])
+	while (lexer->str[i] && lexer->str[i + 1])
 	{
-		if (lexer->token[i] == DOLLAR && lexer->token[i + 1] == WORD)
+		if (lexer->token[i] == HEREDOC)
 		{
-			lexer->str[i][0] = '\a';
-			lexer->token[i] = NA_VALUE;
 			i++;
-			var = find_in_env(lexer->str[i], environ);
-			free(lexer->str[i]);
-			if (var)
-				found_var(lexer, i, var);
-			else
-				no_var(lexer, i);
+			while (lexer->str[i] && lexer->token[i] == SEPARATOR)
+				i++;
+			if (lexer->str[i] && lexer->token[i] == DOLLAR)
+				lexer->token[i] = WORD;
 		}
+		else if (lexer->token[i] == DOLLAR && lexer->token[i + 1] != WORD)
+			lexer->token[i] = WORD;
+		else if (lexer->token[i] == DOLLAR && lexer->token[i + 1] == WORD
+			&& lexer->str[i + 1][0] == '?')
+			handle_dollar_exit(lexer, &i);
+		else if (lexer->token[i] == DOLLAR && lexer->token[i + 1] == WORD)
+			handle_dollar_word(lexer, &i, envp);
 		i++;
 	}
+	if (lexer->str[i] && lexer->token[i] == DOLLAR)
+		lexer->token[i] = WORD;
 }

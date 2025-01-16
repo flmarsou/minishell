fdbec3d3d3b60befcd1bdbacfde240ad25143f98
flmarsou <marsouinflorent12@gmail.com>
2025-01-16 09:50:22 +0100

    Fixed expander bug

--- src/parser/handle_words.c
+++ src/parser/handle_words.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 13:55:53 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/12/13 13:27:59 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/01/16 09:48:14 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,8 @@ static void	union_words(t_lexer *lexer)
 static void	count_quoted_words(t_lexer lexer, unsigned int i,
 	unsigned int *length, unsigned int *nbr_of_token)
 {
-	while (lexer.str[i] && lexer.token[i] == WORD)
+	while (lexer.str[i] && (lexer.token[i] == WORD
+		|| lexer.token[i] == NA_VALUE))
 	{
 		*length += ft_strlen(lexer.str[i]);
 		(*nbr_of_token)++;
@@ -101,12 +102,10 @@ void	handle_words(t_lexer *lexer)
 		nbr_of_token = 0;
 		if (lexer->token[i] == SINGLE_QUOTE || lexer->token[i] == DOUBLE_QUOTE)
 		{
-			lexer->str[i][0] = '\a';
 			lexer->token[i++] = NA_VALUE;
 			count_quoted_words(*lexer, i, &length, &nbr_of_token);
 			edit_lexer(lexer, i, length, nbr_of_token);
 			i += nbr_of_token;
-			lexer->str[i][0] = '\a';
 			lexer->token[i++] = NA_VALUE;
 		}
 		else

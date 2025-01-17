15cfd74566383b3f13525880f3a0cd3e0f2c9b90
flmarsou <marsouinflorent12@gmail.com>
2025-01-17 14:24:09 +0100

    Update ft_export

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -6,48 +6,64 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 13:10:02 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/01/16 15:58:42 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/01/17 14:22:11 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	bubble_sort(char **env)
+static void	add_var(char ***env, char *str)
 {
-	char			**sorted_tab;
-	bool			swapped;
 	unsigned int	i;
+	unsigned int	j;
+	char			**new_env;
 
-	sorted_tab = export_copy_arr(env);
-	swapped = true;
-	while (swapped)
-	{
-		swapped = false;
-		i = 0;
-		while (sorted_tab[i] && sorted_tab[i + 1])
-		{
-			if (strcmp(sorted_tab[i], sorted_tab[i + 1]) > 0)
-			{
-				export_swap_vars(&sorted_tab[i], &sorted_tab[i + 1]);
-				swapped = true;
-			}
-			i++;
-		}
-	}
 	i = 0;
-	while (sorted_tab[i])
-		export_print(sorted_tab[i++]);
-	free(sorted_tab);
+	while ((*env)[i])
+		i++;
+	new_env = malloc(sizeof(char *) * (i + 2));
+	i = 0;
+	j = 0;
+	while ((*env)[i])
+	{
+		new_env[j] = ft_strdup((*env)[i]);
+		i++;
+		j++;
+	}
+	new_env[j] = ft_strdup(str);
+	new_env[j + 1] = NULL;
+	free_env(*env);
+	*env = new_env;
 }
 
-static void	modify_env(char ***env, char *input)
+static void	edit_env(char ***env, char *str)
 {
-	
+	unsigned int	len;
+	unsigned int	i;
+
+	len = 0;
+	i = 0;
+	while (str[i] && str[i] != '=')
+	{
+		len++;
+		i++;
+	}
+	i = 0;
+	while ((*env)[i])
+	{
+		if (ft_strncmp((*env)[i], str, len))
+		{
+			free((*env)[i]);
+			(*env)[i] = ft_strdup(str);
+			return ;
+		}
+		i++;
+	}
+	add_var(env, str);
 }
 
 void	ft_export(char ***env, char **input, unsigned int nbr_of_cmd)
 {
-	unsigned int	j;
 	unsigned int	i;
 
 	i = 1;
@@ -58,15 +74,10 @@ void	ft_export(char ***env, char **input, unsigned int nbr_of_cmd)
 	}
 	while (i < nbr_of_cmd)
 	{
-		if (input[i] && input[i][0] == '=') // TODO: Add conditions for naming
-		{
-			write(1, "\e[1;97mExport: '=': not a valid identifier!\n\e[0m", 48);
-			return ;
-		}
+		if (!check_input(input[i]))
+			i++;
 		else
-		{
-			// TODO: Add if not already exist
-		}
+			edit_env(env, input[i]);
 		i++;
 	}
 }

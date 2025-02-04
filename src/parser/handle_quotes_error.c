22831256b5b1795586e2159f78749ab260a2e133
flmarsou <marsouinflorent12@gmail.com>
2025-02-04 09:49:14 +0100

    Added newlines

--- src/parser/handle_quotes_error.c
+++ src/parser/handle_quotes_error.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 12:55:45 by flmarsou          #+#    #+#             */
-/*   Updated: 2025/02/04 09:46:56 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/02/04 09:48:39 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ bool	handle_quotes_error(t_lexer lexer)
 		i++;
 	}
 	if (quote == SINGLE_QUOTE)
-		return (printf(ERR"Syntax error, unexpected `'`"), false);
+		return (printf(ERR"Syntax error, unexpected `'`\n"), false);
 	else if (quote == DOUBLE_QUOTE)
-		return (printf(ERR"Syntax error, unexpected `\"`"), false);
+		return (printf(ERR"Syntax error, unexpected `\"`\n"), false);
 	return (true);
 }

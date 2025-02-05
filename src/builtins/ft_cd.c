809d63365e1d0a8102f48db6f4802a7ab7498476
flmarsou <marsouinflorent12@gmail.com>
2025-02-05 15:09:57 +0100

    Update ft_cd

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,15 +6,54 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 17:26:28 by andi              #+#    #+#             */
-/*   Updated: 2025/02/04 11:44:50 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/02/05 13:51:07 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_cd(char **command, unsigned int nbr_of_cmd)
+static char	*get_home_var(char **env)
 {
-	(void)command;
-	(void)nbr_of_cmd;
-	printf("WIP\n");
+	unsigned int	i;
+	char			*var;
+	bool			found_var;
+
+	i = 0;
+	found_var = false;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], "HOME=", 5))
+		{
+			var = env[i] + 5;
+			found_var = true;
+		}
+		i++;
+	}
+	if (!found_var)
+	{
+		printf(ERR"$HOME not found!\n");
+		g_exit_status = 1;
+		return (NULL);
+	}
+	return (var);
+}
+
+void	ft_cd(char **command, unsigned int nbr_of_cmd, char **env)
+{
+	char	*home_var;
+
+	home_var = get_home_var(env);
+	if (!home_var)
+		return ;
+	if (nbr_of_cmd == 1)
+		chdir(home_var);
+	else
+	{
+		if (chdir(command[1]) == -1)
+		{
+			printf(ERR"Not a directory!\n");
+			g_exit_status = 1;
+			return ;
+		}
+	}
 }

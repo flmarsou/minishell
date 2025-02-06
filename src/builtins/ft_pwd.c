4e1ebcb2d9682b9ab866988722a5a28f72f583b5
flmarsou <marsouinflorent12@gmail.com>
2025-02-06 09:58:18 +0100

    Stack to Heap allocation

--- src/builtins/ft_pwd.c
+++ src/builtins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 16:54:08 by andi              #+#    #+#             */
-/*   Updated: 2025/02/04 09:57:45 by flmarsou         ###   ########.fr       */
+/*   Updated: 2025/02/06 09:33:30 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 
 void	ft_pwd(void)
 {
-	char	current_path[PATH_MAX];
+	char	*cwd;
 
-	if (!getcwd(current_path, sizeof(current_path)))
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
 	{
-		printf(ERR"Function getcwd failed!\n");
+		printf(ERR"Failed to get current working directory!\n");
 		exit(1);
 	}
 	else
 	{
-		write(1, &current_path, ft_strlen(current_path));
-		write(1, "\n", 1);
+		printf("%s\n", cwd);
+		free(cwd);
 	}
 	exit(0);
 }
